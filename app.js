22fe59bd173f9bd04047008559e279bda156f79b
Alexander Song <alex.ky.song@gmail.com>
2019-03-04 15:40:46 -0800

    server

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ const port = 1337;
 
 app.use(express.static('website'));
 
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "website", "index.html"));	
+});
+
 let server = http.createServer(app);
 server.listen(port, () => console.log('listening from port 1337'));
